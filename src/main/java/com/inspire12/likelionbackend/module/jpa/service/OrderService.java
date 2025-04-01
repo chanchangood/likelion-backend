6e0fa4eb1a030f1400f4bb103949bf82a55ced89
chanchangood <chanwoo.kim31@gmail.com>
2025-04-01 16:10:29 +0900

    jpa를 api로 실습

--- src/main/java/com/inspire12/likelionbackend/module/jpa/service/OrderService.java
+++ src/main/java/com/inspire12/likelionbackend/module/jpa/service/OrderService.java
@@ -21,6 +21,8 @@ public class OrderService {
     public OrderResponse getOrder(Long orderId) {
         OrderEntity order = orderJpaRepository.findById(orderId)
                 .orElseThrow(() -> new EntityNotFoundException("주문 없음"));
+//        OrderEntity order = orderJpaRepository.findById(orderId)
+//            .orElse(new OrderEntity());// 이렇게 값을 찾지 못하면 그냥 텅빈 엔티티를 생성해서 리턴해버려도 됨.
         return OrderMapper.fromEntity(order);
     }
 
@@ -34,7 +36,7 @@ public class OrderService {
     @Transactional
     public void deleteOrder(Long orderId) {
         OrderEntity order = orderJpaRepository.findById(orderId)
-                .orElseThrow(() -> new EntityNotFoundException("주문 없음"));
+                .orElseThrow(() -> new EntityNotFoundException("주문 없음"));//체이닝 방식
         orderJpaRepository.delete(order);
     }
 
@@ -43,6 +45,7 @@ public class OrderService {
         OrderEntity order = orderJpaRepository.findById(orderId)
                 .orElseThrow(() -> new EntityNotFoundException("주문 없음"));
         order.changeTotalAmount(newAmount);
+        // .save()를 하지 않았지만 @Transactional이 붙어있기 때문에 PersistenceContext에 엔티티가 저장되고 변경사항이 db에 적용된다.
         return OrderMapper.fromEntity(order);
     }
 }
